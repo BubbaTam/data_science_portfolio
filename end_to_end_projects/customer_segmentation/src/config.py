d5080a353f651ae5fbd684f72efa0ec514e654ab
BubbaTam <jamesglassey@outlook.com>
2021-12-04 15:53:08 +0000

    add DATA_FILE location

--- end_to_end_projects/customer_segmentation/src/config.py
+++ end_to_end_projects/customer_segmentation/src/config.py
@@ -1,3 +1,4 @@
-TRAINING_FILE = 'data\mall_customers_raw.csv'
+DATA_FILE = 'data'
+RAW_DATA_FILE = 'data\mall_customers_raw.csv'
 MODEL_OUTPUT = 'saved_entities\models'
 PARAMETERS_OUTPUT = 'saved_entities\parameters'
