4b14e65f6de16b4c8965ab03de2339bc93403c3b
BubbaTam <jamesglassey@outlook.com>
2022-01-27 18:29:18 +0000

    class to clean column names

--- end_to_end_projects/segmentation/customer_segmentation/src/features/data_cleaning.py
+++ end_to_end_projects/segmentation/customer_segmentation/src/features/data_cleaning.py
@@ -16,6 +16,32 @@ from joblib import dump
 from dataclasses import dataclass
 
 
+processed_features = ['gender','age','annual_income_(k$)','spending_score_(1-100)']
+processed_dependent_var = None
+processed_independent_var = processed_features.remove(processed_dependent_var)
+
+processed_continuous_var = ['age','annual_income_(k$)','spending_score_(1-100)']
+processed_nominal_var = ['gender']
+
+
+@dataclass
+class RawDataCleaning():
+    column_names : list = None
+    independent_var : list = None
+    dependent_var : list = None
+    continuous_var : list = None
+    nominal_var : list = None
+    ordinal_var : list = None
+
+    def qol_column_names(dataframe):
+        """[current iteration puts the column labels into lowercase and replaces a spaces with underscores]
+
+        Args:
+            dataframe ([pandas dataframe])
+        """
+        dataframe.columns = [column.lower().replace(' ','_').replace("-","-").replace("?","_").replace("/","_").replace("\\","_").replace("%","_") \
+                                    .replace("(","").replace(")","").replace("$","") for column in dataframe.columns]
+
 
 
 
