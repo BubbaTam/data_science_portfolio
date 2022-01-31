73c5a398dedea3963241d21ef767cd63a31df9aa
BubbaTam <jamesglassey@outlook.com>
2022-01-31 10:51:12 +0000

    Release 1.0.0 flask script

---
+++ end_to_end_projects/segmentation/customer_segmentation/src/deployment/app.py
@@ -0,0 +1,47 @@
+import os
+import numpy as np
+import joblib
+import pandas as pd
+from src.config import MODEL_OUTPUT, PARAMETERS_OUTPUT
+from flask import Flask, render_template,jsonify, request
+
+
+flask_app = Flask(__name__)
+
+model = joblib.load(os.path.join(MODEL_OUTPUT,'kmeans_model.pkl'))
+
+
+@flask_app.route("/")
+@flask_app.route("/home")
+def home():
+    return render_template('home.html')
+
+
+
+@flask_app.route("/predict",methods=["POST"])
+def predict():
+    ID_data = request.form["ID"]
+    gender_input = request.form["Gender"]
+    age_input = request.form["Age"]
+    income_input = request.form["Income"]
+    spending_input = request.form["Spending_score"]
+    values = [ID_data,gender_input,age_input,income_input,spending_input]
+
+
+    # data preprocessing
+    df = pd.DataFrame({'gender':[gender_input],'age':[age_input],'annual_income_(k$)':[income_input],'spending_score_(1-100)':[spending_input]})
+    gender_mapping = {'Female':0,'Male':1}
+    df.gender = df.gender.replace(gender_mapping)
+
+    scaler = joblib.load(os.path.join(PARAMETERS_OUTPUT,'min_max_scaler.bin'))
+    df.iloc[:,1:] = scaler.transform(df.iloc[:,1:])
+
+    prediction = model.predict(df)
+    return render_template("home.html", inputs = values, prediction = prediction)
+
+@flask_app.route("/predict_API",methods=["POST"])
+def predict_API():
+    """ to input a json data and to predict from the model"""
+    pass
+
+flask_app.run(debug=True)
