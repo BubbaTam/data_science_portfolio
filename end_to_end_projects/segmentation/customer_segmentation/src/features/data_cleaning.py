8aaefba4cb946ae9003c8e2c064149233391bf45
BubbaTam <jamesglassey@outlook.com>
2022-02-03 09:19:14 +0000

    Perf [feat]: clean Feature Identification class

--- end_to_end_projects/segmentation/customer_segmentation/src/features/data_cleaning.py
+++ end_to_end_projects/segmentation/customer_segmentation/src/features/data_cleaning.py
@@ -1,13 +1,17 @@
-#what to do
+#Possible to look at:
 # quality of life changes
 # missing data
-import os
 
-from pandas.core.frame import DataFrame
-from scipy.sparse import data 
+#current implementations
+
+
+#future improvements
+
+
+import os
 from src.config import PARAMETERS_OUTPUT
-import pandas as pd 
-import numpy as np 
+import pandas as pd
+import numpy as np
 from sklearn.preprocessing import OneHotEncoder,OrdinalEncoder,LabelEncoder ,MinMaxScaler, StandardScaler, Normalizer
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
@@ -15,33 +19,14 @@ from typing import Optional
 from joblib import dump
 from dataclasses import dataclass
 
-
-processed_features = ['gender','age','annual_income_(k$)','spending_score_(1-100)']
-processed_dependent_var = None
-processed_independent_var = processed_features.remove(processed_dependent_var)
-
-processed_continuous_var = ['age','annual_income_(k$)','spending_score_(1-100)']
-processed_nominal_var = ['gender']
-
-
 @dataclass
-class RawDataCleaning():
-    column_names : list = None
-    independent_var : list = None
-    dependent_var : list = None
-    continuous_var : list = None
-    nominal_var : list = None
-    ordinal_var : list = None
-
-    def qol_column_names(dataframe):
-        """[current iteration puts the column labels into lowercase and replaces a spaces with underscores]
-
-        Args:
-            dataframe ([pandas dataframe])
-        """
-        dataframe.columns = [column.lower().replace(' ','_').replace("-","-").replace("?","_").replace("/","_").replace("\\","_").replace("%","_") \
-                                    .replace("(","").replace(")","").replace("$","") for column in dataframe.columns]
-
+class FeatureIdentification():
+    features : list = None
+    independent_feat : list = None
+    dependent_feat : str = None
+    continuous_feat : list = None
+    nominal_feat : list = None
+    ordinal_feat : list = None
 
 
 
