8b0e700b2e9e3a4f99b74fe7105054bb1f9d6a43
BubbaTam <jamesglassey@outlook.com>
2022-02-10 17:00:57 +0000

    feat: Make a file with constant variables

---
+++ end_to_end_projects/segmentation/customer_segmentation/src/config.py
@@ -0,0 +1,5 @@
+DATA_FOLDER = 'data'
+RAW_DATA_FILE = 'data\mall_customers_raw.csv'
+PROCESSED_DATA_FILE = 'data\mall_customers_processed.csv'
+MODEL_OUTPUT = 'saved_entities\models'
+PARAMETERS_OUTPUT = 'saved_entities\parameters'
